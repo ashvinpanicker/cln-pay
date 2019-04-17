383fb5b9e6b917349ba68c4b076a30f698ed7373
Ashvin Panicker <ashvin@lastbit.io>
2019-04-17 06:17:46 +0300

    Transactions

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var utils = require('../utils');
 var express = require("express");
 var router = express.Router();
 var lightning = require("lightning-client");
@@ -9,13 +10,40 @@ router.get("/", async (req, res, next) => {
     let info = await _lightning.getinfo();
     let connectToNode = info.id + '@' + info.address[0].address + ':' + info.address[0].port;
 
-    let invoices = await _lightning.listinvoices().invoices;
+    let { invoices } = await _lightning.listinvoices();
+    let { payments } = await _lightning.listpayments();
+    let transactions = [];
+
+    if (invoices)
+        invoices.map(invoice => {
+            if (invoice.status == "paid") {
+                transactions.push({
+                    time: utils.timeConverter(invoice.paid_at),
+                    type: 'received',
+                    amount: invoice.amount_received_msat,
+                    desc: invoice.description
+                });
+            }
+        });
+
+    if (payments)
+        payments.map(payment => {
+            if (payment.status == "paid") {
+                transactions.push({
+                    time: utils.timeConverter(payment.paid_at),
+                    type: 'sent',
+                    amount: payment.amount_sent_msat,
+                    desc: payment.description
+                });
+            }
+        });
+
     await res.render("index", {
         title: "Ashvin's Lightning Node ⚡️",
         availableBalance: 0,
         connectToNode,
         ...info,
-        ...invoices
+        transactions
     });
 });
 
