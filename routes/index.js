c402ff733d3a6aa76239c922d7fb9fad7d9c1c77
Ashvin Panicker <ashvin@lastbit.io>
2019-04-17 05:22:16 +0300

    Display CLN info

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ var config = require("../config");
 var lightningPath = config.lightningPath;
 var _lightning = new lightning(lightningPath, true);
 
-router.get("/", (req, res, next) => {
-    res.render("index", {
+router.get("/", async (req, res, next) => {
+    let info = await _lightning.getinfo();
+    let connectToNode = info.id + '@' + info.address[0].address + ':' + info.address[0].port;
+    let feesInSatoshis = info.msatoshi_fees_collected / 1000;
+    await res.render("index", {
         title: "Ashvin's Lightning Node ⚡️",
-        availableBalance: 0
+        availableBalance: 0,
+        connectToNode,
+        feesInSatoshis,
+        ...info
     });
-})
+});
 
 module.exports = router;
