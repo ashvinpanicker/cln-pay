8a7a4bc65b649803879143f7599c3f2278de897d
Ashvin Panicker <ashvin@lastbit.io>
2019-04-17 05:34:09 +0300

    Init Jquery

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,14 @@ var _lightning = new lightning(lightningPath, true);
 router.get("/", async (req, res, next) => {
     let info = await _lightning.getinfo();
     let connectToNode = info.id + '@' + info.address[0].address + ':' + info.address[0].port;
-    let feesInSatoshis = info.msatoshi_fees_collected / 1000;
+
+    let invoices = await _lightning.listinvoices().invoices;
     await res.render("index", {
         title: "Ashvin's Lightning Node ⚡️",
         availableBalance: 0,
         connectToNode,
-        feesInSatoshis,
-        ...info
+        ...info,
+        ...invoices
     });
 });
 
